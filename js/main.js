8e63a1ce9aeef331b2ccf2a45b1805099e4942f0
Kicker <kickerock4@gmail.com>
2018-02-05 22:37:35 +0200

    Updated main
    Main.js from Tfiger landing page.  NavBar , PopUp, Thumbs changer.

--- js/main.js
+++ js/main.js
@@ -3,28 +3,61 @@
 // Email: kickerwrk@gmail.com
 // 2018
 
+
 $(document).ready(function() {
-  $(".owl-carousel").owlCarousel({
+
+  $("#owlone").owlCarousel({
     items: 1,
     loop: true,
     dots: false,
     nav: true,
     navText: ["<img src='img/prevarrow.png'>", "<img src='img/nextarrow.png'>"],
     mouseDrag: false,
+    URLhashListener: true,
     merge: false
   });
+
+  $("#owl2").owlCarousel({
+    items: 1,
+    loop: true,
+    dots: false,
+    nav: true,
+    navText: ["<img src='img/prevarrow.png'>", "<img src='img/nextarrow.png'>"],
+    mouseDrag: true,
+    merge: false
+  });
+
+  $("#owlthree").owlCarousel({
+    items: 1,
+    loop: true,
+    dots: false,
+    nav: true,
+    navText: ["<img src='img/prevarrow.png'>", "<img src='img/nextarrow.png'>"],
+    mouseDrag: true,
+    merge: false
+  });
+
   $('a[href*=\\#]:not([href=\\#])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.selector == '#razmer') return;
+      if (target.selector == '#modality') return;
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top
+          scrollTop: target.offset().top + 50
         }, 1000);
         return false;
       }
     }
+  });
+  // slide Changer
+  $('a.prod').click(function() {
+    var target = $("#buy");
+    $('html,body').animate({
+      scrollTop: target.offset().top +100
+    }, 1000);
+
+
   });
   // Thumbs Changer
   $('.thumbs>div').click(function() {
@@ -40,5 +73,21 @@ $(document).ready(function() {
       }, 500);
     }
   });
+
+  // PopUp changer
+  $('a[data-target="#modality"]').click(function() {
+    var imgSrc = $('#owlone .active').find('.main__img').attr('src');
+    var nameSrc = $('#owlone .active').find('.buy__header').text();
+    $('#modality img').attr('src', imgSrc);
+    $('#modality .modal__text').text(nameSrc);
+
+
+  });
+
+if($(window).width() < 600) {
+  $('.navbar-toggler').show();
+  $('#navb1').addClass('collapse');
+}
+
 
 });
